5c37331eb842f5b7fe2c5a47264e6869d5b1e658
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2026-01-19 17:26:56 +0600

    Fixed extra whitespace issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ const App = () => {
   return (
     <div
       style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
+        width: "100%",
+        height: "100%",
       }}
     >
       <BusTracker />
